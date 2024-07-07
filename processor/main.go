5e632a160d5a8305f0ed018dd509e6d018055005
Cade Cuddy <cadecuddy@gmail.com>
2024-07-07 01:28:35 -0500

    Implement playlist track fetching in playlist processor

--- processor/main.go
+++ processor/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/zmb3/spotify"
 )
+
+const PLAYLIST_ENDPOINT = "https://api.spotify.com/v1/playlists/"
+
+type PlaylistMessage struct {
+	PlaylistId  string `json:"playlistId"`
+	AccessToken string `json:"accessToken"`
+}
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -17,7 +28,7 @@ func main() {
 	rabbitmqUser := os.Getenv("RABBITMQ_DEFAULT_USER")
 	rabbitmqPass := os.Getenv("RABBITMQ_DEFAULT_PASS")
 
-	conn, connErr := amqp.Dial("amqp://" + rabbitmqUser + ":" + rabbitmqPass + "@rabbitmq:5672/")
+	conn, connErr := amqp.Dial("amqp://" + rabbitmqUser + ":" + rabbitmqPass + "@rabbitmq")
 	failOnError(connErr, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -57,7 +68,58 @@ func main() {
 
 	go func() {
 		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
+			var message PlaylistMessage
+			err := json.Unmarshal(d.Body, &message)
+			if err != nil {
+				log.Printf("Error unmarshalling message: %s", err)
+				d.Nack(false, true)
+				continue
+			}
+
+			client := &http.Client{}
+			req, err := http.NewRequest("GET", PLAYLIST_ENDPOINT+message.PlaylistId+"/tracks?offset=0&limit=100", nil)
+			if err != nil {
+				log.Printf("Error creating request: %s", err)
+				d.Nack(false, true)
+				continue
+			}
+			req.Header.Set("Authorization", "Bearer "+message.AccessToken)
+
+			var tracks []spotify.PlaylistTrack
+			offset := 0
+
+			for {
+				resp, err := client.Do(req)
+				if err != nil {
+					log.Printf("Error making request: %s", err)
+					d.Nack(false, true)
+					break
+				}
+				defer resp.Body.Close()
+
+				if resp.StatusCode != http.StatusOK {
+					log.Printf("Non-200 response: %d", resp.StatusCode)
+					d.Nack(false, true)
+					break
+				}
+
+				var playlistTracks spotify.PlaylistTrackPage
+				err = json.NewDecoder(resp.Body).Decode(&playlistTracks)
+				if err != nil {
+					log.Printf("Error decoding response: %s", err)
+					d.Nack(false, true)
+					break
+				}
+
+				tracks = append(tracks, playlistTracks.Tracks...)
+				if playlistTracks.Next == "" {
+					break
+				}
+				offset += 100
+				req.URL.RawQuery = "offset=" + strconv.Itoa(offset) + "&limit=100"
+			}
+
+			log.Printf("%s - Total tracks: %d", message.PlaylistId, len(tracks))
 			d.Ack(false)
 		}
 	}()
